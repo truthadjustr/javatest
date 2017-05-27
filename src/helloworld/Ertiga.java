6c8c45c985d896f382f461714e3ff5e7e42f6f08
dexter aparicio <syscools@gmail.com>
2017-05-27 23:25:38 +0800

    first commit

---
+++ src/helloworld/Ertiga.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class Ertiga {
+	Ertiga() {
+		System.out.println("Ertiga conx");
+	}
+	
+	void f1(int n) {
+		System.out.println("value = " + n);
+	}
+}
---
+++ src/helloworld/MainApp.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ertiga objTest = new Ertiga();
+		
+		for (int i = 0;i < 5; i++) {
+			//System.out.println("i = " + i);
+			objTest.f1(i);
+		}
+	}
+
+}
